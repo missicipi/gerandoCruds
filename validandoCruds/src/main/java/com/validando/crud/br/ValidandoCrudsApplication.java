485c25b355c2158e1072e21b9d75916447f1aa24
unknown <professorulissesm@gmail.com>
2020-02-13 20:44:21 -0300

    Iniciando o Sistema

---
+++ validandoCruds/src/main/java/com/validando/crud/br/ValidandoCrudsApplication.java
@@ -0,0 +1,13 @@
+package com.validando.crud.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidandoCrudsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidandoCrudsApplication.class, args);
+	}
+
+}
---
+++ validandoCruds/src/test/java/com/validando/crud/br/ValidandoCrudsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validando.crud.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidandoCrudsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
